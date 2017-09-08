4ddd9a917d9efa3b11f4d0df65d46483e4d68bb7
Rento <rento@rentodesign.com>
2017-09-08 12:51:30 +0100

    added example favicon.ico

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ export default (props) => (
     <Head>
       <title>Latest tweets</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel='shortcut icon' type='image/x-icon' href='/static/favicon.ico' />
       <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css' />
     </Head>
 
